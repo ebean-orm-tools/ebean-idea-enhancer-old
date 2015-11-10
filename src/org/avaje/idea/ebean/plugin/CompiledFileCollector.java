f8e45d2f0f51207770c7ec90c81671817201de72
Robin Bygrave <robin.bygrave@gmail.com>
2015-11-11 10:29:40 +1300

    Just change to use LinkedHashMap to preserve ordering

--- src/org/avaje/idea/ebean/plugin/CompiledFileCollector.java
+++ src/org/avaje/idea/ebean/plugin/CompiledFileCollector.java
@@ -23,100 +23,72 @@ import com.intellij.openapi.compiler.CompilationStatusListener;
 import com.intellij.openapi.compiler.CompileContext;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
 
 /**
  * @author yevgenyk - Updated 28/04/2014 for IDEA 13
  */
 public class CompiledFileCollector implements CompilationStatusListener {
-    private Map<String, File> compiledClasses = new HashMap<>();
+
+  private LinkedHashMap<String, File> compiledClasses = new LinkedHashMap<>();
+
+  @Override
+  public void fileGenerated(String outputRoot, String relativePath) {
+    // Collect all valid compiled '.class' files
+    final CompiledFile compiledFile = createCompiledFile(outputRoot, relativePath);
+    if (compiledFile != null) {
+      this.compiledClasses.put(compiledFile.className, compiledFile.file);
+    }
+  }
+
+  private CompiledFile createCompiledFile(String outputRoot, String relativePath) {
+
+    if (outputRoot == null || relativePath == null || !relativePath.endsWith(".class")) {
+      return null;
+    }
+
+    final File file = new File(outputRoot, relativePath);
+    if (!file.exists()) {
+      return null;
+    }
+
+    final String className = resolveClassName(relativePath);
+
+    return new CompiledFile(file, className);
+  }
+
+  /**
+   * Given a content path and a class file path, resolve the fully qualified class name
+   */
+  private String resolveClassName(String relativePath) {
+    final int extensionPos = relativePath.lastIndexOf('.');
+    return relativePath.substring(0, extensionPos).replace('/', '.');
+  }
+
+  @Override
+  public void compilationFinished(boolean aborted,
+                                  int errors,
+                                  int warnings,
+                                  CompileContext compileContext) {
+    new EbeanEnhancementTask(compileContext, compiledClasses).process();
+    this.compiledClasses = new LinkedHashMap<>();
+  }
+
+  public static class CompiledFile {
+    private final File file;
+    private final String className;
+
+    private CompiledFile(File file, String className) {
+      this.file = file;
+      this.className = className;
+    }
 
     @Override
-    public void fileGenerated(String outputRoot, String relativePath) {
-        // Collect all valid compiled '.class' files
-        final CompiledFile compiledFile = createCompiledFile(outputRoot, relativePath);
-        if (compiledFile != null) {
-            this.compiledClasses.put(compiledFile.className, compiledFile.file);
-        }
+    public String toString() {
+      return "CompiledFile{" +
+          "file=" + file +
+          ", className='" + className + '\'' +
+          '}';
     }
-
-    private CompiledFile createCompiledFile(String outputRoot, String relativePath) {
-        if (outputRoot == null || relativePath == null || !relativePath.endsWith(".class")) {
-            return null;
-        }
-
-        final File file = new File(outputRoot, relativePath);
-        if (!file.exists() || !isJavaClass(file)) {
-            return null;
-        }
-
-        final String className = resolveClassName(relativePath);
-
-        return new CompiledFile(file, className);
-    }
-
-    /**
-     * Given a content path and a class file path, resolve the fully qualified class name
-     */
-    private String resolveClassName(String relativePath) {
-        final int extensionPos = relativePath.lastIndexOf('.');
-        return relativePath.substring(0, extensionPos).replace('/', '.');
-    }
-
-    /**
-     * Check if the file is a java class by peeking the first two magic bytes and see if we need a 0xCAFE ;-)
-     */
-    private boolean isJavaClass(File file) {
-        try (InputStream is = new FileInputStream(file)) {
-            final byte[] buf = new byte[2];
-            final int read = is.read(buf, 0, 2);
-            if (read < buf.length) {
-                return false;
-            }
-            return buf[0] == (byte) 0xCA &&
-                   buf[1] == (byte) 0xFE;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    @Override
-    public void compilationFinished(boolean aborted,
-                                    int errors,
-                                    int warnings,
-                                    CompileContext compileContext) {
-        new EbeanEnhancementTask(compileContext, compiledClasses).process();
-        this.compiledClasses = new HashMap<>();
-    }
-
-    public static class CompiledFile {
-        private final File file;
-        private final String className;
-
-        private CompiledFile(File file, String className) {
-            this.file = file;
-            this.className = className;
-        }
-
-        public File getFile() {
-            return file;
-        }
-
-        public String getClassName() {
-            return className;
-        }
-
-        @Override
-        public String toString() {
-            return "CompiledFile{" +
-                "file=" + file +
-                ", className='" + className + '\'' +
-                '}';
-        }
-    }
+  }
 }
